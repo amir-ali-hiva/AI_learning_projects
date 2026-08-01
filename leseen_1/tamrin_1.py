ca476a7dece871e4eca9c098bf8186c50f17a8ab
amir-ali-hiva <Amiralirahimian3@gmail.com>
2026-08-01 15:30:07 +0330

    1.1

--- leseen_1/tamrin_1.py
+++ leseen_1/tamrin_1.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 f = lambda x : 2 * x + 5
 
@@ -8,3 +9,4 @@ number_of_data = 20
 x = np.linspace (0 , 100 , number_of_data)
 y = f(x)
 
+y_noiss = np.random.rand(number_of_data, 1) * 10 + y
